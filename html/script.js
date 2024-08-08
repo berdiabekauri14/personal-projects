126a952456d528fb4c4558f6d5e0190dceb58188
berdiabekauri14 <berdiabekauri5@gmail.com>
2024-08-08 11:44:54 +0400

    august

---
+++ html/script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector("div");
+let input = document.querySelector("input");
+
+input.addEventListener("input", () => {
+    box.style.borderRadius = input.value;
+    box.style.background = input.value;
+    box.style.boxShadow = input.value;
+});
